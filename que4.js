b912d73b353e7b4cb6cbfb225e6eaea97043e798
Vrushank2808 <vrushankgotawala@gmail.com>
2024-10-24 18:47:22 +0530

    added sorting and dry run each code

--- que4.js
+++ que4.js
@@ -11,3 +11,24 @@ var singleNumber = function(nums) {
     }
     return result;  
 };
+
+/*
+nums = [5, 1, 5]
+result = 0 (this will store the final single number)
+
+Loop 1:
+  i = 0
+  result = result ^ nums[0] => result = 0 ^ 5 => result = 5
+
+Loop 2:
+  i = 1
+  result = result ^ nums[1] => result = 5 ^ 1 => result = 4
+
+Loop 3:
+  i = 2
+  result = result ^ nums[2] => result = 4 ^ 5 => result = 1
+
+End of loop: result = 1 (this is the single number)
+
+Final result: 1
+*/
